50abecdf8dd3a4ed7c78ede508309800c6d59b06
Sulav Baral <sulavbaral58@gmail.com>
2022-09-21 17:50:33 +0545

    scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 
 
 import {  HashRouter as  BrowserRouter , Route , Routes } from 'react-router-dom'
